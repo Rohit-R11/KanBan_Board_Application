6ef7298a4e5dd774836793c4a6d522f4981ba44a
Rohit-R11 <101731213+Rohit-R11@users.noreply.github.com>
2022-06-01 13:54:25 +0530

    Delete notificationapplication directory

--- notificationapplication/src/main/java/com/niit/notificationapplication/service/MessageService.java
+++
@@ -1,9 +0,0 @@
-package com.niit.notificationapplication.service;
-
-
-import com.niit.notificationapplication.model.Task;
-
-public interface MessageService {
-
-    public abstract void delTask(Task task);
-}
