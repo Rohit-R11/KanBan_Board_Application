0fd856f5fb83138a1e910a312ca8de65e09d5209
Rohit-R11 <101731213+Rohit-R11@users.noreply.github.com>
2022-06-01 13:53:35 +0530

    Delete authenticationapplication directory

--- authenticationapplication/src/main/java/com/niit/authenticationapplication/services/AuthenticationTokenGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.niit.authenticationapplication.services;
-
-import com.niit.authenticationapplication.model.User;
-
-import java.util.Map;
-
-public interface AuthenticationTokenGenerator {
-    public abstract Map<String, String> generateToken(User user);
-}
